78e0f269d99a712cd8e42b34b6b30e485498da81
App Academy Student <student@appacademy.io>
2015-12-08 14:53:22 -0800

    Finish CRUD - finally...

--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -42,9 +42,108 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	TodoStore = __webpack_require__(1);
+
+/***/ },
+/* 1 */
 /***/ function(module, exports) {
 
-
+	var _todos = [],
+	    _callbacks = [];
+	
+	var TodoStore = {
+	
+	  addChangeHandler: function (cb) {
+	    _callbacks.push(cb);
+	  },
+	
+	  removeChangeHandler: function (cb) {
+	    var index = _callbacks.indexOf(cb);
+	
+	    if (index === -1) {
+	      return;
+	    }
+	
+	    _callbacks.splice(index, 1);
+	  },
+	
+	  changed: function () {
+	    _callbacks.forEach(function (cb) {
+	      cb();
+	    });
+	  },
+	
+	  all: function () {
+	    return _todos.slice();
+	  },
+	
+	  fetch: function () {
+	    $.get('api/todos', {}, function (todos) {
+	      _todos = todos;
+	      TodoStore.changed();
+	    });
+	  },
+	
+	  create: function (todo) {
+	    // do we need reference to todo in our anonymous function
+	    $.post('api/todos', { todos: todo }, function () {
+	      _todos.push(todo);
+	      TodoStore.changed();
+	    });
+	  },
+	
+	  destroy: function (id) {
+	    var deleteItemIdx = this.find(id);
+	
+	    if (deleteItemIdx !== -1) {
+	      $.ajax({
+	        url: "/api/todos/" + id,
+	        type: 'DELETE',
+	        success: function () {
+	          _todos.splice(deleteItemIdx, 1);
+	          TodoStore.changed();
+	        }
+	      });
+	    }
+	  },
+	
+	  find: function (id) {
+	    for (var i = 0; i < _todos.length; i++) {
+	      if (_todos[i].id === id) {
+	        return i;
+	      }
+	    }
+	  },
+	
+	  updateDone: function (id) {
+	    var updateItemIdx = this.find(id);
+	    var currentTodo = _todos[updateItemIdx];
+	
+	    if (updateItemIdx !== -1) {
+	      $.ajax({
+	        url: "/api/todos/" + id,
+	        data: { todos: currentTodo },
+	        type: "PATCH",
+	        success: (function () {
+	          this.toggleDone(updateItemIdx);
+	          TodoStore.changed();
+	        }).bind(this)
+	      });
+	    }
+	  },
+	
+	  toggleDone: function (toggleIdx) {
+	    if (_todos[toggleIdx].done === true) {
+	      _todos[toggleIdx].done = false;
+	    } else {
+	      _todos[toggleIdx].done = true;
+	    }
+	  }
+	};
+	
+	module.exports = TodoStore;
 
 /***/ }
 /******/ ]);
