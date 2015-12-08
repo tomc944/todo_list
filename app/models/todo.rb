e465ad79efe9944113b18a432f24a3157683608c
App Academy Student <student@appacademy.io>
2015-12-08 12:01:24 -0800

    Almost done with CRUD

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,4 +1,4 @@
 class Todo < ActiveRecord::Base
   validates :title, :body, presence: true
-  validates :done, inclusion: { in: %w(true false)}
+  validates :done, inclusion: { in: [true, false]}
 end
