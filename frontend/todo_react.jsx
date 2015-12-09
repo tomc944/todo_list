1361a17de2bb50d526e6846c47a7dccf3d6ed55c
App Academy Student <student@appacademy.io>
2015-12-08 16:55:39 -0800

    Finish displaying TODOS - YAHOO

--- frontend/todo_react.jsx
+++ frontend/todo_react.jsx
@@ -1,1 +1,11 @@
-TodoStore = require('./stores/todo_store.js');
+var TodoList = require("./components/todo_list.jsx"),
+    React = require('react'),
+    ReactDOM = require('react-dom');
+
+    // window.TodoStore = require("./stores/todo_store");
+document.addEventListener('DOMContentLoaded', function () {
+  ReactDOM.render(
+    <TodoList/>,
+    document.getElementById('root')
+  );
+});
